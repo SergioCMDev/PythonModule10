bdea36256a81584fe6d12b37fa7ce1d0095f020d
sergiocm <sergiocm@github.com>
2026-04-20 17:16:04 +0200

    Mejoras ex1

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -1,13 +1,46 @@
 from collections.abc import Callable
+from typing import Any
 
+def condition_function_true() -> bool:
+    return True
+
+def condition_function_false() -> bool:
+    return False
 
 def main() -> None:
-	pass
+	combined = spell_combiner(heal, hit)
+	print(combined("Orc", 30))
 
-def spell_combiner(spell1: Callable, spell2: Callable) -> Callable
-def power_amplifier(base_spell: Callable, multiplier: int) -> Callable
-def conditional_caster(condition: Callable, spell: Callable) -> Callable
-def spell_sequence(spells: list[Callable]) -> Callable
+	amplifier = power_amplifier(heal, 5)
+	print(amplifier("Orc", 30))
+	conditional = conditional_caster(condition_function_true, heal)
+	print(conditional("Orc", 30))
+	conditional2 = conditional_caster(condition_function_false, heal)
+	print(conditional2("Orc", 30))
+
+def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
+	def combined_spell(*args: Any, **kwargs: Any) -> tuple[Any, Any]:
+		result1 = spell1(*args, **kwargs)
+		result2 = spell2(*args, **kwargs)
+		return(result1, result2)
+	return combined_spell
+
+def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+	def amplified_spell(target: str, power: int) -> Any:
+		return base_spell(target, power * multiplier)
+	return amplified_spell
+
+
+def conditional_caster(condition: Callable, spell: Callable) -> Callable:
+	def conditional_spell(*args: Any, **kwargs: Any) -> Callable:
+		if condition():
+			return spell(*args, **kwargs)
+		else:
+			return "Spell fizzled"
+	return conditional_spell
+
+def spell_sequence(spells: list[Callable]) -> Callable:
+	pass
 
 
 def spell(target: str, power: int) -> str:
@@ -15,6 +48,15 @@ def spell(target: str, power: int) -> str:
 
 def heal(target: str, power: int) -> str:
 	return f"Heal restores {target} for {power} HP"
+
+
+def hit(target: str, power: int) -> str:
+	return f"Hit damages {target} for {power} HP"
+
+def multiplier(target: str, power: int) -> str:
+	return f"Heal restores {target} for {power} HP"
+
+
 
 if __name__ == "__main__":
     main()
